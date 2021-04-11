242cc38c29cb3a30b20498cb79faf6bfcce607a6
ibartuszek <istvan.bartuszek@gmail.com>
2021-04-11 18:20:09 +0200

    First commit

---
+++ src/test/kotlin/com/example/chatapp/chatapp/ChatAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chatapp.chatapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
