7bb75613b9b5f8d56973d9e326a290a9b1526166
ibartuszek <istvan.bartuszek@gmail.com>
2021-04-12 15:22:13 +0200

    client: Add basic form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,47 @@
+import { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Chat application</h1>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "John Doe",
+      message: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(event) {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(`Send message: name=${this.state.name} message=${this.state.message}`);
+  }
+
+  render() {
+    return (
+      <div className="body">
+        <h1>Chat application</h1>
+        <hr/>
+        <form className="w-75 mx-auto py-3 row" onSubmit={this.handleSubmit}>
+          <label className="col-form-label">Message</label>
+          <input type="text" className="form-control col ml-3"
+                  value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" value="Send" className="btn btn-light ml-3 col-3" />
+        </form>
+      </div>
+    );
+  }
+
 }
 
 export default App;
