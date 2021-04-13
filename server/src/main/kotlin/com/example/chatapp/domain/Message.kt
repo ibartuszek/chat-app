7996f1871c0b52bd9a3a7a5c4c675494ea126cd0
ibartuszek <istvan.bartuszek@gmail.com>
2021-04-13 21:34:23 +0200

    websocket: Add date to Message

--- server/src/main/kotlin/com/example/chatapp/domain/Message.kt
+++ server/src/main/kotlin/com/example/chatapp/domain/Message.kt
@@ -1,3 +1,5 @@
 package com.example.chatapp.domain
 
-class Message(var name: String, var message: String)
+import java.time.Instant
+
+class Message(var name: String, var message: String, var date: Instant)
