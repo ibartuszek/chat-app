e837c3676fea27a22e9a979ee25877c3fba46f39
Istvan.Bartuszek <istvan.bartuszek@betvictor.com>
2021-05-21 15:18:53 +0200

    chat-server: implement findByUserId

--- chat-server/src/main/kotlin/com/example/chatapp/service/topic/TopicService.kt
+++ chat-server/src/main/kotlin/com/example/chatapp/service/topic/TopicService.kt
@@ -1,29 +1,28 @@
 package com.example.chatapp.service.topic
 
-import com.example.chatapp.dal.repository.TopicRepository
+import com.example.chatapp.dal.repository.TopicDao
 import com.example.chatapp.domain.NewTopicRequest
 import com.example.chatapp.domain.TopicModel
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
-class TopicService(private val transformer: TopicTransformer, private val repository: TopicRepository) {
+class TopicService(private val transformer: TopicTransformer, private val dao: TopicDao) {
 
     companion object {
         private val log = LoggerFactory.getLogger(TopicService::class.java)
     }
 
     fun create(topicRequest: NewTopicRequest): TopicModel {
-        val savedTopic = repository.save(transformer.transform(topicRequest))
+        val savedTopic = dao.save(transformer.transform(topicRequest))
         log.info("New topic created by user=${topicRequest.ownerId} name=${topicRequest.name}")
         return transformer.transform(savedTopic)
     }
 
     fun findByUserId(userId: String): List<TopicModel> {
-        // should return by ownerId and userId
-        val topicList = listOf(createMockTopic())
+        val topicList = dao.findTopicsByUserId(userId)
         log.info("Return topicList by userId=$userId size=${topicList.size}")
-        return topicList
+        return topicList.map { transformer.transform(it) }
     }
 
     fun update(topic: TopicModel): TopicModel {
@@ -37,15 +36,5 @@ class TopicService(private val transformer: TopicTransformer, private val reposi
         // should set it not active, reading can be allowed
         log.info("Topic id=$id is closed by userId=$ownerId")
     }
-
-    // should delete after implementation
-    private fun createMockTopic() = TopicModel.Builder()
-            .id("fake topic id")
-            .name("fake topic name")
-            .ownerId("fake user id")
-            .userIds(listOf("another fake user id"))
-            .messages(emptyList())
-            .active(true)
-            .build()
 
 }
